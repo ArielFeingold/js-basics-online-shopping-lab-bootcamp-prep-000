5495e8923b6bd114a91bee190d0b904e446f99b5
Ariel Feingold <feingold.ariel@gmail.com>
2018-01-04 19:32:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,9 +61,11 @@ function removeFromCart(item){
  }
 
 function placeOrder(cardNumber) {
-  if(cardNumber === null){
-    return `Sorry, we don't have a credit card on file for you.`
+  if(cardNumber === undefined){
+    console.log(`Sorry, we don't have a credit card on file for you.`)
   } else{
-    return `Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+    cart = []
+    return cart
   }
 }
