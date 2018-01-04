7876d214f647388b6456ac3d2ea5fe619eff5856
Ariel Feingold <feingold.ariel@gmail.com>
2018-01-04 19:26:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -44,18 +44,21 @@ function total() {
   return total
 }
 
-function removeFromCart(item) {
-  for(var i = 0; i < cart.length; i++){
-    var name = cart[i]
-    if(name.hasOwnProperty(item)){
-      delete cart[i]
-      return cart
-    } else{
-      console.log("That item is not in your cart.")
-      return cart
-    }
-  }
-}
+function removeFromCart(item){
+ 
+   for(var i = 0, l = cart.length; i < l; i++){
+ 
+   for(var list in cart[i]){
+       if(item === list){
+         cart.splice(i,1)
+         return cart
+        }
+     }
+   }
+ 
+   console.log("That item is not in your cart.")
+   return cart
+ }
 
 
 function placeOrder(cardNumber) {
